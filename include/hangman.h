bb03ed5676dd2314a3fa55ef974c9b2b4460e9b5
Julien VIGNAU-ESPINE <julien.vignau-espine@epitech.eu>
2022-05-10 14:53:55 +0200

    add: include file + structure

--- include/hangman.h
+++ include/hangman.h
@@ -2,10 +2,23 @@
 ** EPITECH PROJECT, 2022
 ** hangman
 ** File description:
-** stumper5
+** stumper 7
 */
 
 #ifndef HANGMAN_H_
     #define HANGMAN_H_
+
+    typedef struct game_s {
+	int tries;
+	char *secret_word;
+	char *player_word;
+    } game_t;
+
+    void clean_str_end(char *str);
+    void clean_str_start(char *str);
+    int find_space(char *str);
+    char **my_str_to_word_array(char *str, char delim);
+    int open_file(const char *path);
+    char **read_file(const char *path);
 
 #endif /* HANGMAN_H_ */
