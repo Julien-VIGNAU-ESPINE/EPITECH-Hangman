04dedbcc1c2351ad78ff2b925a430a1d9848045e
Julien VIGNAU-ESPINE <julien.vignau-espine@epitech.eu>
2022-05-10 17:13:14 +0200

    add: new my_put_nbr

--- src/my_put_nbr.c
+++ src/my_put_nbr.c
@@ -9,17 +9,22 @@
 
 int my_put_nbr(int nb)
 {
+    int i;
     char c;
 
     if (nb < 0) {
         write(1, "-", 1);
-        nb *= -1;
+        nb = nb * (-1);
     }
-    if (nb == 0) {
-        return 0;
+    if (nb >= 10) {
+        i = (nb % 10);
+        nb = (nb - i) / 10;
+        my_put_nbr(nb);
+        c = 48 + i;
+        write(1, &c, 1);
+    } else {
+        c = 48 + nb % 10;
+        write(1, &c, 1);
     }
-    my_put_nbr(nb / 10);
-    c = (nb % 10 + '0');
-    write(1, &c, 1);
-    return 0;
+    return (0);
 }
